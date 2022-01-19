a10c4f14589f07d4aaed581788450979f295f732
Leonardo Cherin <leocherin@gmail.com>
2022-01-19 22:09:11 +0000

    Adding supporting files

---
+++ IntroStuff.py
@@ -0,0 +1,38 @@
+
+def startup():
+    print("""                                                          :*:                                
+                                                                                    :VM$$NF*                              
+                                :*VV**                                         :*M$NVVMNMNMV:                           
+                            :*FN$$MVNF*         ***:                        :FN$$F*VNF:::*MN*                          
+                            :VNMM$$$$NN$$F:    :VM$$MMF*:                   *FN$$$$MN$V      *FF:                        
+                        *F$$N*M$FF$N$$$NNF*:VN$$N$$M$$$MV:             :*M$$$$$$$$V::     :V**NF:                      
+                        :*M$$$$$N$F*M$FM$$FVN$$FF$MFN$VF$$$$MV:         *VN$$$$$$$$$$F**VMNMMN$$NN$N*                     
+                    :VM$$$$NFVVM$$F*M$$$$$$$MVM$NFFFFM$FM$MNMMFFVV*VFM$NN$$$$$$$$$$$$$$$$$$$$MVM$$$V                    
+                    :FN$$$NFF*:*M$$$N* *VN$$$$$MVV:     ::::*VFFVVFN$$$$$MN$$$MN$$$$$$$$$$$$FM$$$$$$$$V                   
+                *MMF$$M*  :VN$NF*::     *FV*V:                    :*M$$$$$$NM$$$$$$$$$$$$$$NMF$$$$$$$V                  
+                :VN$NNFV: VNNMV*:                              ::::*VFN$$$$$$$$$$$$VM$$$$$$$$$$N$$NNFMMMF:                
+            :VMMFFV*  :VF*:                     :***VVV**VFMMVVVM$$$MM$$$$$$$$$$$M$$$$VN$$$$$$$$$MFVV*MNF*              
+            *::    :***:                    :**M$$FM$$$NN$$NN$$$$$$$MFN$$$$$$$$$$$$$$$M$$$$$$$$$$N$$N$$$MFV:            
+                *VV*::        ::*VFFMFV*****VF*FN$$$$$F**N$$NN$$$NMV*VM$$$$$$$$$$$$$$$$$$MF*FNNN$M*F$MN$$M$$MV*::        
+            :::::          *M$NV****F$$$$FVFN$$$$$$$MVMN$$$$$$$MFVVM$$$$NF***FFMNNNNMFV: ::::::*VV*FMMMMMMNNMFFMMMMV*   
+            ::            :VM$$$$NNFVF$NMM$NFVM$$$N$$$FF$$$$$$$$F*VN$$F*VV:        ::                        :    ::::    
+                    :VFN$NV**::   ::: ::****:::****VVMN$$$$$$$M**FNM*:                                                 
+                    :****MF:                              :VFFVV***::                                                     
+                :*VV* :VFV                                                                                                
+    :::::  :*VMNMMV*:                                                                                                    
+    :*VFFFVVV*::                                                                                                         
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                                                                                                        
+                                    :*     :     :    :     :      :     :: :   ::    ::                                 
+                                    :F**:   V*   :F:   FF*   F:     V:    FF V   :VV  VV                                  
+                                    :F *:   V*   :F:   F:*V* F:     V:    FVV:     *VV:                                   
+                                    :F*V    :V*:*V*    F:  *VM:     V:    F*        **                                    
+                                    :* ::     :::      :     :      :     :         ::                                    
+                                                                                                                        
+                                                                                                                        
+                                                    :*******************                                                  
+                                                                                                                        
+                                                                                                                        """)
